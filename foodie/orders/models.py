8758f1572377efca0d94073b477989c1e41b1b6e
matias <matiasmdelp@gmail.com>
2019-10-08 23:30:02 -0300

    Que se listen solo las ordenes pertinentes

--- foodie/orders/models.py
+++ foodie/orders/models.py
@@ -10,16 +10,10 @@ from foodie.users.models import User
 class Order(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     notes = models.CharField(max_length=100, null=True)
-<<<<<<< Updated upstream
-    delivery_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="delivered_orders",
-                                      limit_choices_to={'is_delivery': True})
-    client_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders_made",
-=======
     # TODO: DeliveryUser and ClientUser shouldn't be User subclasses instead of a flag?
     delivery_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='delivered_orders',
                                       limit_choices_to={'is_delivery': True})
     client_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders_made',
->>>>>>> Stashed changes
                                     limit_choices_to={'is_delivery': False})
     date_time_ordered = models.DateTimeField(auto_now_add=True)
 
--- foodie/orders/views.py
+++ foodie/orders/views.py
@@ -1,6 +1,6 @@
-# Create your views here.
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, status
 from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from foodie.orders.models import Product, Order, Item
 from foodie.orders.serializers import ProductSerializer, ItemSerializer, OrderSerializer
@@ -19,21 +19,23 @@ class OrderViewSet(mixins.CreateModelMixin,
                    mixins.ListModelMixin,
                    viewsets.GenericViewSet):
     permission_classes = (OrderPermissions,)
-    queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
-    def create(self, request, is_delivery):
+    def get_queryset(self):
+        if self.request.user.is_delivery:
+            queryset = self.request.user.delivered_orders.all()
+        else:
+            return self.request.user.orders_made.all()
+
+    def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_error=True)
+        serializer.is_valid(raise_exception=True)
 
         serializer.save(client_user=request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class ItemViewSet(mixins.CreateModelMixin,
-                  mixins.RetrieveModelMixin,
-                  mixins.ListModelMixin,
                   viewsets.GenericViewSet):
     permission_classes = (ItemPermissions,)
-    queryset = Item.objects.all()
     serializer_class = ItemSerializer
