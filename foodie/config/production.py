d847fa83aa51176759afbe7e64607164d1283da7
matias <matiasmdelp@gmail.com>
2019-10-14 20:15:20 -0300

    Fix database url

--- foodie/config/production.py
+++ foodie/config/production.py
@@ -2,6 +2,7 @@ from foodie.config.common import *
 
 import django_heroku
 django_heroku.settings(locals())
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # HTTPS
 SECURE_SSL_REDIRECT = True
