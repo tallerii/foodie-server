e99e746a8494cb47252de6d3266580b3e91b1ea4
Matías Marcó del Pont <matiasmdelp@gmail.com>
2019-12-01 22:30:14 -0300

    Add address string to order

--- foodie/orders/serializers.py
+++ foodie/orders/serializers.py
@@ -4,18 +4,16 @@ from foodie.users.serializers import PublicUserSerializer
 from .models import Order
 
 
-class ListOrdersSerializer(GeoFeatureModelSerializer):
+class ListOrdersSerializer(serializers.ModelSerializer):
     client_user = PublicUserSerializer(required=False)
     delivery_user = PublicUserSerializer(required=False)
 
     class Meta:
         model = Order
-        geo_field = 'actual_location'
-
         fields = ('id', 'notes', 'delivery_user', 'client_user', 'date_time_ordered', 'delivery_price',
-                  'price', 'start_location', 'end_location', 'actual_location', 'status')
+                  'price', 'start_location', 'start_address', 'end_location', 'end_address', 'status')
 
 class CreateOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ('notes', 'price', 'start_location', 'end_location')
+        fields = ('notes', 'price', 'start_location', 'start_address', 'end_location', 'end_address')
